c85a591c242ceb9703bb83249ea8bbbe52db47d9
zainabbajwa1 <117822226+zainabbajwa1@users.noreply.github.com>
2022-12-07 21:21:34 +0500

    I store website

---
+++ script.js
@@ -0,0 +1,27 @@
+// Count down start
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+let countDown = () => {
+    let futureDate = new Date("12 december2022");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
+
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
+// count doun end
